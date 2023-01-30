43ec3d8b27bf64de975efff3ac2242abb6639d1d
ElieZoetaba <zoetabaelie@gmail.com>
2023-01-30 15:12:20 +0000

    let getQuote
    Signed-off-by: ElieZoetaba <zoetabaelie@gmail.com>

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,3 +3,12 @@ let author = document.getElementById("author");
 let btn = document.getElementById("btn");
 
 const url = "https://api.quotable.io/random";
+
+let getQuote = () => {
+  fetch(url)
+    .then((data) => data.json())
+    .then((item) => {
+      quote.innerText = item.content;
+      author.innerText = item.author;
+    });
+};
